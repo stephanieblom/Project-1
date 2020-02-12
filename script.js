19dc2ac71259791272489cb6a025da279c313ca6
Stephanie Blom <stephblom@hotmail.com>
2020-02-12 11:43:34 -0500

    Updated html page with Jquery src

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
-let ingredients = [];
 
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi",
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
-		"x-rapidapi-key": "3b8a7d5c9dmsh20b4f77d73d4977p127a4fjsndd1100cc381e",
-		"content-type": "text/plain",
-		"accept": "text/plain"
-	},
-	"data": "https://www.thewholesomedish.com/the-best-classic-chili/"
+let name = "";
+let imgURL = ""
+let ingredients = [];
+let instructions = [];
+
+
+
+function dataPull(){
+    console.log(`[data:Pull]`);
+    var recipeURL = $('#recipeURL').val();
+    console.log(`recipe URL: ${recipeURL}`);
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi",
+        "method": "POST",
+        "headers": {
+            "x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
+            "x-rapidapi-key": "3b8a7d5c9dmsh20b4f77d73d4977p127a4fjsndd1100cc381e",
+            "content-type": "text/plain",
+            "accept": "text/plain"
+        },
+        "data": `${recipeURL}`
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        console.log(response[0].ingredients);
+        console.log(response[0].instructions[0].steps);
+    
+        name = response[0].name;
+        imgURL = response[0].images[0];
+        ingredients = response[0].ingredients;
+        instructions = response[0].instructions[0].steps;
+    
+    
+    
+        $(`.imgContent`).append(`<img src="${imgURL}" alt="">`)
+    
+        for(var i=0; i < ingredients.length; i++){
+    
+            $('#ingredientList').append(`<li>${ingredients[i]}</li>`);
+    
+        }
+    
+        for(var i=0; i < instructions.length; i++){
+    
+            $('#instructionsList').append(`<li>${instructions[i]}</li>`);
+    
+        }
+    
+        nutritionInfo();
+    
+    });
+
 }
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-    console.log(response[0].ingredients);
-    console.log(response[0].instructions[0].steps);
-
-    let name = response[0].name;
-    let img = response[0].images[0];
-    ingredients = response[0].ingredients;
-    let instructions = response[0].instructions[0].steps;
-
-
-
-    $(`#img`).append(`<h1>${name}</h1><br><img src="${img}" attr="Image of completed recipe"/>`)
-
-    for(var i=0; i < ingredients.length; i++){
-
-        $('#ingredientList').append(`<li>${ingredients[i]}</li>`);
-
-    }
-
-    for(var i=0; i < instructions.length; i++){
-
-        $('#instructionsList').append(`<li>${instructions[i]}</li>`);
-
-    }
-
-    nutritionInfo();
-
-});
 
 function nutritionInfo(){
 
@@ -67,3 +81,9 @@ function nutritionInfo(){
     });
 
 }
+
+$.ready(function(){
+
+$(`.submitBtn`).on("click", dataPull);
+
+});
